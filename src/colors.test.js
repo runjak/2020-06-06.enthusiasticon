78e490e7544a466626d0757d40fd08b76af93619
Jakob Runge <sicarius@g4t3.de>
2020-05-23 21:37:03 +0200

    add colorsOnFace and applyPermutation to colors module

--- src/colors.test.js
+++ src/colors.test.js
@@ -1,9 +1,53 @@
-import { solvedColors } from "./colors";
+import {
+  Color,
+  solvedColors,
+  Face,
+  colorsOnFace,
+  applyPermutation,
+} from "./colors";
+import { shuffle } from "./permutations";
 
 describe("colors", () => {
   describe("solvedColors", () => {
     it("should have the correct number of colors", () => {
       expect(solvedColors.length).toBe(6 * 9);
+    });
+  });
+
+  describe("colorsOnFace", () => {
+    it("should yield the correct color to face combination for solvedColors", () => {
+      const combinations = [
+        ["U", Color.white],
+        ["F", Color.green],
+        ["R", Color.red],
+        ["B", Color.blue],
+        ["L", Color.orange],
+        ["D", Color.yellow],
+      ];
+
+      combinations.forEach(([face, color]) => {
+        const colors = colorsOnFace(solvedColors, face);
+
+        expect(colors.length).toBe(9);
+        expect(colors.every((c) => c === color)).toBe(true);
+      });
+    });
+  });
+
+  describe("applyPermutation", () => {
+    it("should apply the permutation for R as expected", () => {
+      const actual = applyPermutation(solvedColors, shuffle(["R"]));
+      // prettier-ignore
+      const expected = [
+        Color.white, Color.white, Color.green, Color.white, Color.white, Color.green, Color.white, Color.white, Color.green,
+        Color.green, Color.green, Color.yellow, Color.green, Color.green, Color.yellow, Color.green, Color.green, Color.yellow,
+        Color.red, Color.red, Color.red, Color.red, Color.red, Color.red, Color.red, Color.red, Color.red,
+        Color.white, Color.blue, Color.blue, Color.white, Color.blue, Color.blue, Color.white, Color.blue, Color.blue,
+        Color.orange, Color.orange, Color.orange, Color.orange, Color.orange, Color.orange, Color.orange, Color.orange, Color.orange,
+        Color.yellow, Color.yellow, Color.blue, Color.yellow, Color.yellow, Color.blue, Color.yellow, Color.yellow, Color.blue,
+      ];
+
+      expect(actual).toEqual(expected);
     });
   });
 });
--- src/colors.ts
+++ src/colors.ts
@@ -1,4 +1,6 @@
-enum Color {
+import { Permutation } from "./permutations";
+
+export enum Color {
   white = "white",
   green = "green",
   red = "red",
@@ -15,3 +17,27 @@ export const solvedColors = [
   Color.orange,
   Color.yellow,
 ].flatMap((c: Color): Array<Color> => Array(9).fill(c));
+
+export type Face = "U" | "L" | "F" | "R" | "B" | "D";
+
+const faceSlices: { [face in Face]: [number, number] } = {
+  U: [0, 9],
+  F: [9, 18],
+  R: [18, 27],
+  B: [27, 36],
+  L: [36, 45],
+  D: [45, 54],
+};
+
+export const colorsOnFace = (
+  colors: Array<Color>,
+  face: Face
+): Array<Color> => {
+  const [start, end] = faceSlices[face];
+  return colors.slice(start, end);
+};
+
+export const applyPermutation = (
+  colors: Array<Color>,
+  permutation: Permutation
+): Array<Color> => permutation.map((i) => colors[i]);
